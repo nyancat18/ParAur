0df6d22ce3c5190fea40fb10cca5d62195f1e998
Juan <gatitofeliz@protonmail.com>
2016-12-21 21:39:13 -0400

    h

--- dope.sh
+++ dope.sh
@@ -1,3 +1,5 @@
 #bin/sh
-wget https://repomirror.parabola.nu/pool/parabola_gnu+linux/iceweasel-1:50.1.0.deb1-1-i686.pkg.tar.xz
-wget https://repomirror.parabola.nu/pool/parabola_gnu+linux/iceweasel-1:50.1.0.deb1-1-'x86_64'.pkg.tar.xz
+pack=icedove
+ver=45.5.1
+wget https://repomirror.parabola.nu/pool/parabola_gnu+linux/$pack-1:$ver.deb1-1-i686.pkg.tar.xz
+wget https://repomirror.parabola.nu/pool/parabola_gnu+linux/$pack-1:$ver.deb1-1-x86_64.pkg.tar.xz
