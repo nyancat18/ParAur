15bd02c0d9c0551664856bd267f4a532c4360518
Juan <gatitofeliz@protonmail.com>
2016-12-21 21:33:42 -0400

    icedove

---
+++ dope.sh
@@ -0,0 +1,3 @@
+#bin/sh
+wget https://repomirror.parabola.nu/pool/parabola_gnu+linux/iceweasel-1:50.1.0.deb1-1-i686.pkg.tar.xz
+wget https://repomirror.parabola.nu/pool/parabola_gnu+linux/iceweasel-1:50.1.0.deb1-1-'x86_64'.pkg.tar.xz
