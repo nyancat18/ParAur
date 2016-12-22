2e085049c52b33b3a94697e28af7516852d0a728
Juan <gatitofeliz@protonmail.com>
2016-12-21 21:52:28 -0400

    g

---
+++ ia.sh
@@ -0,0 +1,4 @@
+#bin/sh
+
+wget https://repomirror.parabola.nu/pool/parabola_gnu+linux/iceape-1:2.40-5-x86_64.pkg.tar.xz
+wget https://repomirror.parabola.nu/pool/parabola_gnu+linux/iceape-1:2.40-5-i686.pkg.tar.xz
