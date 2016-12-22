f43006fd93ac25f3761554355d3ff17e2cf8220e
Juan <gatitofeliz@protonmail.com>
2016-12-21 22:11:33 -0400

    hj

---
+++ ll.sh
@@ -0,0 +1,4 @@
+#bin/sh
+
+wget https://repomirror.parabola.nu/pool/parabola_gnu+linux/linux-libre-4.8.14_gnu-1-i686.pkg.tar.xz
+wget https://repomirror.parabola.nu/pool/parabola_gnu+linux/linux-libre-4.8.14_gnu-1-x86_64.pkg.tar.xz
